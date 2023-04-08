2c313973ff3cf518b6ea4c1cd07a69eba2bacc03
RomanMorf <kostromichov.roman@gmail.com>
2023-04-08 13:28:45 +0300

    asdasdas

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/my_pet/',
+      path: '/',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/my_pet/about',
+      path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    
+
   ]
 })
 
