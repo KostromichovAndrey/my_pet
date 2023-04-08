fa6936c6e7c3f7deed0c945c9fb3e70bf1a6a910
RomanMorf <kostromichov.roman@gmail.com>
2023-04-08 13:55:17 +0300

    small changes

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/my_pet/' : './',
-  // base: '/my_pet/'
+  base: '/my_pet/'
 })
