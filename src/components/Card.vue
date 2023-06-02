ff9297dbb1efd23467964dec97bf07ed2eb676de
RomanMorf <kostromichov.roman@gmail.com>
2023-06-02 18:24:29 +0300

    card create

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  cardInfo: Object
+})
+
+</script>
+
+<template>
+  <div class="card">
+    <img class="card-img" v-bind:src="cardInfo.url" alt="">
+    <h3 class="card-title">{{ cardInfo.title }}</h3>
+    <p class="card-descr">{{ cardInfo.title }}</p>
+    <button>Add to compare list</button>
+    <button>Add to favore</button>
+    <button>Add to cart</button>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+  .card {
+    border: 1px solid black;
+    border-radius: 10px;
+
+    &-img {
+      max-width: 150px;
+      max-height: 200px;
+    }
+
+    &-title {
+      padding: 10px;
+    }
+
+    &-descr {
+
+    }
+  }
+</style> 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,49 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import Card from '../components/Card.vue';
+
+const cardList = ref([])
+
+const fetchData = async () => {
+  fetch('https://jsonplaceholder.typicode.com/photos?_page=2&_limit=12')
+  .then(response => response.json())
+  .then(products => {
+
+    console.log(products, 'products')
+    cardList.value = products;
+  })
+}
+
+onMounted(() => {
+  fetchData();
+})
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <div v-if="cardList.length" class="card-list">
+      <Card v-for="item in cardList" 
+      :key="item"
+      v-bind:card-info="item"
+      />
+    </div>
+
   </div>
 </template>
 
 <style>
+  .card-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
+  }
+
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 50vh;
     display: flex;
-    align-items: center;
+    align-items: center
   }
 }
 </style>
