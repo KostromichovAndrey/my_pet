8482ead506747ff593ff20c9ad92e35c3f32c11a
Kostromichov <Kostromichov.andrej@gmail.com>
2023-11-29 11:29:51 +0200

    seve progress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,90 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+const firstName = "Andrey";
+const secondName = "Kostromichov";
+
+
+
+const personInfo = (arr = []) => {
+  arr.forEach((person, index) => {
+    
+    person.id = index + 1;
+    person.fullName = fullName(person.firstName, person.secondName)
+
+    console.log(person, 'person');
+    // const user = fullName(person.firstName, person.secondName)
+
+    // console.log(user, 'user');
+
+  })
+}
+
+const persons = [
+  {
+    firstName: "Andrey",
+    secondName: "Kostromichov",
+    age: 33,
+  }, 
+  {
+    firstName: "Roman",
+    secondName: "Kostromichov",
+    age: 36,
+  }, 
+  {
+    firstName: "Ropak",
+    secondName: "Kakashka",
+    age: 32,
+  }, 
+];
+
+const persons2 = [
+  {
+    firstName: "Andrey2",
+    secondName: "Kostromichov",
+    age: 33,
+  }, 
+  {
+    firstName: "Roman2",
+    secondName: "Kostromichov",
+    age: 36,
+  }, 
+  {
+    firstName: "Ropak2",
+    secondName: "Kakashka",
+    age: 32,
+  }, 
+];
+
+
+personInfo(persons);
+personInfo(persons2);
+
+console.log(persons, 'persons');
+console.log(persons2, 'persons2');
+
+
+function fullName(firstName, secondName) {
+  let iuqwetyriuqwyetriuqweyrt = firstName + " " + secondName
+  
+  return iuqwetyriuqwyetriuqweyrt;
+} 
+
+// const foo = fullName("Ropak", "Kakashka");
+
+// const foo2 = fullName("Andrey", "Kostromichov");
+
+// const foo3 = fullName("Roman", "Kostromichov");
+
+// console.log(foo, "foo");
+// console.log(foo2, "foo2");
+// console.log(foo3, "foo3");
+
+// const fullName = () => { 
+//   let foo = firstName + " " +  secondName
+// }
+
+
 </script>
 
 <template>
--- src/views/RomarioView.vue
+++ src/views/RomarioView.vue
@@ -1,6 +1,40 @@
+<script setup>
+
+
+
+// import { onMounted, ref } from 'vue';
+// import Card from '../components/Card.vue';
+
+// const cardList = ref([])
+
+// const fetchData = async () => {
+//   fetch('https://jsonplaceholder.typicode.com/photos?_page=2&_limit=12')
+//   .then(response => response.json())
+//   .then(products => {
+
+//     console.log(products, 'products')
+//     cardList.value = products;
+//   })
+// }
+
+// onMounted(() => {
+//   fetchData();
+// })
+</script>
+
 <template>
-  <div class="romario">
-    <h1>This is an Romario page</h1>
+  <div>
+    <div class="header">
+      <h1>To do list</h1>
+      <input type="text">
+      <button>Add task</button>
+    </div>
+    <div class="body">
+      <input type="checkbox">
+      <span>Task 1</span>
+      <button>Remove</button>
+    </div>
+
   </div>
 </template>
 
